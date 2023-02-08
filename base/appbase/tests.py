d401c276463b5cfe11cbb155cb42932edc79c1c0
Daniel Erick <DanielErick.estudante@gmail.com>
2023-02-08 13:58:32 -0300

    salvando

--- base/appbase/tests.py
+++ base/appbase/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import Client
+from django.urls import reverse
+
+from base.django_assertions import assert_contains
+import pytest
+
+@pytest.fixture
+def resp(client: Client):
+    resp = client.get(reverse('appbase:home'))
+    return resp
+
+
+def test_status_code(resp):
+    assert resp.status_code == 200
+
+
+def test_title(resp):
+    assert_contains(resp, f'<title>Python Pro</title>')
+
+
+def test_home_link(resp):
+    assert_contains(resp, f'href="{reverse("appbase:home")}">Daniel Pro</a>')
--- base/base/_test.py
+++
@@ -1,4 +0,0 @@
-from settings import AWS_ACCESS_KEY_ID
-
-def testando_AWS():
-    assert AWS_ACCESS_KEY_ID
